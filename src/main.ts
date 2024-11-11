5e04f1603df1be4827cca82fcd5c230afd1db15b
supachaisu <180935381+supachaisu@users.noreply.github.com>
2024-11-11 19:20:37 +0700

    10-routing

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { bootstrapApplication, provideProtractorTestingSupport } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
+import routeConfig from "./app/routes";
+import { provideRouter } from "@angular/router";
 
-bootstrapApplication(AppComponent)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)],
+}).catch((err) => console.error(err));
