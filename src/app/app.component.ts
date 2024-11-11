740689761b699d22713b44634dff331ae42b644e
supachaisu <180935381+supachaisu@users.noreply.github.com>
2024-11-11 16:37:56 +0700

    02-HomeComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
+  selector: "app-root",
   standalone: true,
-  selector: 'app-root',
-  template: `<h1>Hello Universe!</h1>`,
-  styleUrls: ['./app.component.css'],
+  imports: [HomeComponent],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'homes';
+  title = "homes";
 }
