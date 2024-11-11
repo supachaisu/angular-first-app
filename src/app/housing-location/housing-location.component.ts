82165082713500c0cebe1de6fc03571b87a8dd13
supachaisu <180935381+supachaisu@users.noreply.github.com>
2024-11-11 19:00:53 +0700

    07-dynamic-template-values

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import { HousingLocation } from "../housing-location";
 
 @Component({
@@ -7,11 +7,20 @@ import { HousingLocation } from "../housing-location";
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+    </section>
   `,
-  styleUrls: ['./housing-location.component.css']
+  styleUrls: ["./housing-location.component.css"],
 })
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
