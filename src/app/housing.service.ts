b27c14a241d32424659256d92710a53fc4696c6e
supachaisu <180935381+supachaisu@users.noreply.github.com>
2024-11-11 19:35:38 +0700

    14-http

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -5,18 +5,18 @@ import { HousingLocation } from "./housing-location";
   providedIn: "root",
 })
 export class HousingService {
-  housingLocationList: HousingLocation[] = [];
+  url = "http://localhost:3000/locations";
 
-  constructor() {}
-
-  getAllHousingLocations(): HousingLocation[] {
-    return this.housingLocationList;
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return (await data.json()) ?? [];
   }
 
-  getHousingLocationById(id: number): HousingLocation | undefined {
-    return this.housingLocationList.find(
-      (housingLocation) => housingLocation.id === id
-    );
+  async getHousingLocationById(
+    id: number
+  ): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return (await data.json()) ?? {};
   }
 
   submitApplication(firstName: string, lastName: string, email: string) {
