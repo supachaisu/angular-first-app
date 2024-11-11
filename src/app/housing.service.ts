e383bd86e60f36531076de6157e920adf1341d36
supachaisu <180935381+supachaisu@users.noreply.github.com>
2024-11-11 19:27:51 +0700

    12-forms

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -12,9 +12,16 @@ export class HousingService {
   getAllHousingLocations(): HousingLocation[] {
     return this.housingLocationList;
   }
+
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find(
       (housingLocation) => housingLocation.id === id
+    );
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`
     );
   }
 }
